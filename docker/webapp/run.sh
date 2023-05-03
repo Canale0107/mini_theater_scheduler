283750fb8c5cc2cccf6362b13514723cfc6ab110
kodera.k.ab@m.titech.ac.jp <kodera.k.ab@m.titech.ac.jp>
2023-05-03 22:46:34 +0900

    flaskの動くコンテナを作成

---
+++ docker/webapp/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Dockerイメージ名とコンテナ名を指定
+DOCKER_IMAGE=minitheater_webapp_image
+CONTAINER_NAME=minitheater_webapp_container
+
+# コンテナを作成して、マウントして、時刻も同期して、scheduler.pyを実行。シェルスクリプトのオプションをそのままオプションにする
+# TODO: ディレクトリ構成を見直してもっといい書き方をする
+# TODO: volumeはdocker-composeに書く
+docker run --rm -v "$(pwd)/../../src:/app/src" -v "$(pwd)/../../data:/app/data" -v /etc/localtime:/etc/localtime:ro -p 3333:3333 --name $CONTAINER_NAME $DOCKER_IMAGE:latest sh -c "cd src/webapp && python app.py"
